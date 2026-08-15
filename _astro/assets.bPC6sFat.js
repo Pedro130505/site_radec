d6271d02d133b4ce9c2c7414ce3fb621fd8d3ce8
arthur <arthur.felipepg@gmail.com>
2026-08-15 20:30:56 -0300

    Updates

---
+++ _astro/assets.bPC6sFat.js
@@ -0,0 +1,1 @@
+const s=e=>e?`/site_radec/${e.startsWith("/")?e.slice(1):e}`:"";export{s as g};
--- _astro/circle-check.CceiBMBE.js
+++ _astro/circle-check.CceiBMBE.js
@@ -3,4 +3,4 @@ import{c}from"./createLucideIcon.9c9WRhnR.js";/**
  *
  * This source code is licensed under the ISC license.
  * See the LICENSE file in the root directory of this source tree.
- */const t=c("CircleCheckBig",[["path",{d:"M21.801 10A10 10 0 1 1 17 3.335",key:"yps3ct"}],["path",{d:"m9 11 3 3L22 4",key:"1pflzl"}]]);export{t as C};
+ */const r=c("CircleCheck",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"m9 12 2 2 4-4",key:"dzmm74"}]]);export{r as C};
