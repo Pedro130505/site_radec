a821cc78bbceb509cc8d8db78bdd1ce7272a226f
arthur <arthur.felipepg@gmail.com>
2026-08-15 20:17:44 -0300

    Updates

---
+++ _astro/assets.Dha5_krD.js
@@ -0,0 +1,6 @@
+import{c as e}from"./createLucideIcon.9c9WRhnR.js";/**
+ * @license lucide-react v0.469.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=e("ChevronRight",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]),n=t=>t?`/site_radec/${t.startsWith("/")?t.slice(1):t}`:"";export{c as C,n as g};
